7e9def7e0f9d125d5cb7b9d389f35f472029148f
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2024-03-08 19:25:51 -0500

    Added wifi connection function

--- install.py
+++ install.py
@@ -1,1 +1,10 @@
 # Put functions here
+import os
+
+def connect:
+    """ Connects machine to WiFI network"""
+    os.system("iwctl device wlan0 set-property Powered on")
+    os.system("iwctl station wlan0 scan")
+    os.system("iwctl staTion wlan0 get-networks")
+    ssid = input("Please enter your SSID >>> ")
+    os.system(f"iwctl station wlan0 connect {ssid}")
