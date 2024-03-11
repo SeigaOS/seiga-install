1d929dac499d94df4dd59c470082e2bb9d6af9b0
Elon <62807180+EloniX-X@users.noreply.github.com>
2024-03-11 17:36:47 +0300

    Update real.go

--- timezones/real.go
+++ timezones/real.go
@@ -4,22 +4,43 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 	_ "time/tzdata"
+
+	"runtime"
 
 	"github.com/charmbracelet/huh"
 	"github.com/pelletier/go-toml"
 )
 
-func setSystemTimezone(timezone string) {
-	zoneinfoPath := "/usr/share/zoneinfo/" + timezone
-	localtimePath := "/etc/localtime"
-	if err := os.Remove(localtimePath); err != nil {
-		log.Fatalf("Failed to remove old timezone link: %v", err)
+// func setSystemTimezone(timezone string) {
+// 	zoneinfoPath := "/usr/share/zoneinfo/" + timezone
+// 	localtimePath := "/etc/localtime"
+// 	if err := os.Remove(localtimePath); err != nil {
+// 		log.Fatalf("Failed to remove old timezone link: %v", err)
+// 	}
+// 	if err := os.Symlink(zoneinfoPath, localtimePath); err != nil {
+// 		log.Fatalf("Failed to create symlink for new timezone: %v", err)
+// 	}
+// }
+
+func setSystemTimezone(timezone string) error {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "linux":
+		cmd = exec.Command("sudo", "ln", "-sf", "/usr/share/zoneinfo/"+timezone, "/etc/localtime")
+	case "darwin":
+		cmd = exec.Command("sudo", "systemsetup", "-settimezone", timezone)
+	default:
+		return fmt.Errorf("setting timezone not supported on this OS")
 	}
-	if err := os.Symlink(zoneinfoPath, localtimePath); err != nil {
-		log.Fatalf("Failed to create symlink for new timezone: %v", err)
+
+	//run
+	if err := cmd.Run(); err != nil {
+		return err
 	}
+	return nil
 }
 
 // /usr/share/zoneinfo
@@ -43,7 +64,7 @@ func main() {
 			continue
 		} else {
 
-			fmt.Println(e.Name())
+			//fmt.Println(e.Name())
 			timezonesplace = append(timezonesplace, e.Name())
 			options = append(options, huh.NewOption[string](e.Name(), e.Name()))
 		}
@@ -58,49 +79,73 @@ func main() {
 		Run()
 
 	fmt.Println("great ?!", timezone)
-	newone, err := os.ReadDir("/usr/share/zoneinfo/" + timezone)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var secondtz string
-	newoptions := make([]huh.Option[string], 0, len(timezonesplace))
+	elpath := "/usr/share/zoneinfo/" + timezone
 
-	for _, e := range newone {
-		fmt.Println(e.Name())
-		newoptions = append(newoptions, huh.NewOption[string](e.Name(), e.Name()))
-	}
-	huh.NewSelect[string]().
-		Title("Pick a timezone.").
-		Options(newoptions...).
-		Value(&secondtz).
-		Run()
-	fmt.Println("great ?!", timezone)
-	f, err := os.Create("config.toml")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	if stat, err := os.Stat(elpath); err == nil && stat.IsDir() {
+		fmt.Println("hey")
+		configFile, err := os.ReadFile("config.toml")
+		var config Timeconf
+		newConfig, err := toml.Marshal(config)
+		newone, err := os.ReadDir("/usr/share/zoneinfo/" + timezone)
+		var secondtz string
+		newoptions := make([]huh.Option[string], 0, len(timezonesplace))
+		for _, e := range newone {
+			//fmt.Println(e.Name())
+			newoptions = append(newoptions, huh.NewOption[string](e.Name(), e.Name()))
+		}
+		huh.NewSelect[string]().
+			Title("Pick a timezone.").
+			Options(newoptions...).
+			Value(&secondtz).
+			Run()
+		f, err := os.Create("config.toml")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if err != nil {
+			//fmt.Println(timezone)
+			log.Fatal(err)
+		}
 
-	configFile, err := os.ReadFile("config.toml")
-	var config Timeconf
-	if err := toml.Unmarshal(configFile, &config); err != nil {
-		panic(err)
+		if err := toml.Unmarshal(configFile, &config); err != nil {
+			panic(err)
+		}
+
+		if err != nil {
+			panic(err)
+		}
+		var realtimezone = timezone + "/" + secondtz
+		config.Place = realtimezone
+		if err := os.WriteFile("config.toml", newConfig, 0644); err != nil {
+			panic(err)
+		}
+		fmt.Println(realtimezone)
+		if err != nil {
+			fmt.Println("ok great theres an error")
+		}
+		fmt.Println("great ?! we've set your timezone as", realtimezone)
+		setSystemTimezone(realtimezone)
+
+	} else {
+
+		var config Timeconf
+		fmt.Println("great?! we've set your path as " + timezone)
+		setSystemTimezone(timezone)
+
+		configFile, err := os.ReadFile("config.toml")
+		config.Place = timezone
+		newConfig, err := toml.Marshal(config)
+		if err != nil {
+			panic(err)
+		}
+		if err := toml.Unmarshal(configFile, &config); err != nil {
+			panic(err)
+		}
+		if err := os.WriteFile("config.toml", newConfig, 0644); err != nil {
+			panic(err)
+		}
+
 	}
 
-	if err != nil {
-		panic(err)
-	}
-
-	var realtimezone = timezone + "/" + secondtz
-	config.Place = realtimezone
-
-	newConfig, err := toml.Marshal(config)
-	if err := os.WriteFile("config.toml", newConfig, 0644); err != nil {
-		panic(err)
-	}
-	fmt.Println(realtimezone)
-	if err != nil {
-
-	}
-	setSystemTimezone(realtimezone)
 }
