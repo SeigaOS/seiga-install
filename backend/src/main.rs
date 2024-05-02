e10b9f304214ad976a1262acb0acd4a976c99ea2
Elon <62807180+EloniX-X@users.noreply.github.com>
2024-05-02 12:40:08 -0400

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -45,11 +45,8 @@ impl App {
         frame.render_widget(self, frame.size());
     }
 
-    /// updates the application's state based on user input
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
