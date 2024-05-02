808ebcad6616baacf18ecd068c83f16626b3160c
Elon <62807180+EloniX-X@users.noreply.github.com>
2024-05-02 10:34:37 -0400

    Update system notification.rs
    added toml grabbing added loops and will upload toml in a sec

--- backend/system notification.rs
+++ backend/system notification.rs
@@ -2,19 +2,34 @@
 
 use sysinfo::{System, Components};
 use std::{process::Command, string};
+use std::{thread, time};
+use serde_derive::Deserialize;
+use std::fs;
+use std::process::exit;
+use toml;
 
+#[derive(Deserialize)]
+struct Data {
+    tempcheck: Tempcheck,
+}
+#[derive(Deserialize)]
+struct Tempcheck {
+    checktimems: u64,
+    checkthresholdcelc: f32,
+}
 
-fn cputils() {
+fn cputils(temp: f32) {
+
     let components = Components::new_with_refreshed_list();
     println!("=> components:");
     for component in &components {
         let component_name = component.label();
         if component_name.contains("tdie") {
-            // println!("{} with temp {:?}C", component_name, component.temperature());
+            println!("{} with temp {:?}C", component_name, component.temperature());
             // println!("{}", component_name);
-            if component_name.contains("tdie6") {
+            if component_name.contains("tdie3") {
                 println!("{}", component.temperature());
-                if component.temperature() > 39.0 {
+                if component.temperature() > temp {
                     let stringfr = format!("display notification \"Your CPU temperature is too high\" with title \"CPU Temp Checker X 900\"");
                     Command::new("osascript")
                         .args(["-e", &stringfr])
@@ -58,10 +73,33 @@ fn main() {
    // println!("line");
     //pmset -g batt 
     //chmod +x setlimitcheck
+
+    let filename = "put the path to a checkfor toml here";
+    let contents = match fs::read_to_string(filename) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read file `{}`", filename);
+            exit(1);
+        }
+    };
+
+    let data: Data = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Unable to load data from `{}`", filename);
+            exit(1);
+        }
+    };
+
     let mut sys = System::new_all();
     sys.refresh_all();
-    cputils();
-    battman();
+    loop {
+        let ten_millis = time::Duration::from_millis(data.tempcheck.checktimems);
+        let now = time::Instant::now();
+        thread::sleep(ten_millis);
+        cputils(data.tempcheck.checkthresholdcelc );
+    };
+    // battman();
     // println!("total memory: {} bytes", sys.total_memory());
     // println!("used memory : {} bytes", sys.used_memory());
     // println!("total swap  : {} bytes", sys.total_swap());
@@ -72,7 +110,7 @@ fn main() {
     // println!("System host name:        {:?}", System::host_name());
     
     // loop {
-    //     sys.refresh_cpu();
+    //     sys.refresh_cpu(); // Refreshing CPU information.
     //     for cpu in sys.cpus() {
     //         print!("{}% ", cpu.cpu_usage());
     //     }
